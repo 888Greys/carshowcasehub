274546f1b9a04928c5b75ba32f8f0e6c6f42f9ba
vincent1037514 <vincent1037514@gmail.com>
2024-10-16 10:11:44 +0300

    Added footer links to Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 
+
+import { footerLinks } from "@/constants";  
+import { link } from "fs";
 const Footer = () => {
     return (
         <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
@@ -14,7 +17,22 @@ const Footer = () => {
                     </p>
                 </div>
                 <div className="footer__links">
-                    
+                    {footerLinks.map((link) => (
+                        <div key={link.title}
+                        className="footer__link">
+                            <h3 className="font-bold">{link.title}</h3>
+                            {link.links.map((item) => (
+                                <Link
+                                    key={item.title}
+                                    href={item.url}
+                                    className="text-gray-500"
+                                    >
+                                        {item.title}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+
                 </div>
             </div>
 
